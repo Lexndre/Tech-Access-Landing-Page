f3e6a1e44c64039575c52a305fa86936cf0ef8d4
Lexndre_kvk <leandrechristemmanuel@gmail.com>
2025-08-13 19:12:58 +0000

    Finish Product Page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,35 +3,38 @@
         <div class="top-products-section">
             <p class="text-intro">Contactez-nous via Email ou nos reseaux</p>
         </div>
-        <div class="left-contact-content">
-            <div class="form">
+        <row class="mid-contact-section">
+            <div class="left-contact-content">
                 <form action="">
-                    <div class="row">
+                    <div class="form-row">
                         <label for="">Your name :</label>
                         <input type="text">
                     </div>
-                    <div class="row">
+                    <div class="form-row">
                         <label for="">Your email :</label>
                         <input type="email">
                     </div>
-                    <div class="row">
+                    <div class="form-row">
                         <label for="">Your message :</label>
                         <textarea name="" id="" cols="30" rows="10"></textarea>
                     </div>
+                    <div class="form-row">
+                        <button class="submit">Submit</button>
+                    </div>
                 </form>
             </div>
-        </div>
-        <div class="right-contact-content">
-            <div class="contact-img">
-                <img src="" alt="">
+            <div class="right-contact-content">
+                <div class="contact-img">
+                    <img src="" alt="">
+                </div>
+                <div class="social-networks">
+                    <row class="row">
+                        <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                    </row>
+                </div>
             </div>
-            <div class="social-networks">
-                <row class="row">
-                    <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
-                </row>
-            </div>
-        </div>
+        </row>
     </section>
